dadb802d02d19099713434dd35002055447c10ae
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2016-01-30 01:06:28 +0800

    Initial project setup.

---
+++ app/src/main/java/sidespell/tech/midtermexam/fragments/MainFragment.java
@@ -0,0 +1,32 @@
+package sidespell.tech.midtermexam.fragments;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sidespell.tech.midtermexam.R;
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // TODO: Find all views here..
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // TODO: Perform logic operations here..
+    }
+}
